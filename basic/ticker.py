98f9fa62cb57c9a2fa8e9b4dbf0ee6fc6895c034
Kim Seon Woo <seonwoo960000@naver.com>
2022-10-04 07:09:43 +0900

    temp

--- basic/ticker.py
+++ basic/ticker.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from functools import reduce
 
 import yfinance as yf
 
@@ -19,6 +20,9 @@ class Ticker:
     def get_close_result(self):
         return self.api_result['Close']
 
+    def get_buy_price(self):
+        return self.get_close_price(self.start)
+
     def get_close_price(self, date):
         try:
             return self.api_result['Close'].iloc.obj[date]
@@ -36,8 +40,36 @@ class Ticker:
         return (end - start) / start
 
 
+class TickerGroup:
+    def __init__(self, tickers):
+        self.tickers = tickers
+        self.names = set()
+        for t in tickers:
+            self.names.add(t['ticker'].ticker_code)
+
+    def get_ticker_names(self):
+        return self.names
+
+    def get_yesterday_total_price(self):
+        yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+        return reduce(lambda x, y: x + y, [t['count'] * t['ticker'].get_close_price(yesterday) for t in self.tickers])
+
+
+def test_ticker_group():
+    apple = Ticker('AAPL', '2020-01-02')
+    microsoft = Ticker('MSFT', '2020-01-02')
+    ticker_group = TickerGroup([
+        {'ticker': apple, 'count': 10},
+        {'ticker': microsoft, 'count': 20}
+    ])
+    print(apple.get_close_price(datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')))
+    print(microsoft.get_close_price(datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')))
+    print(ticker_group.get_yesterday_total_price())
+
+
 def main():
-    print('ticker.py')
+    test_ticker_group()
+
 
 if __name__ == '__main__':
     main()
